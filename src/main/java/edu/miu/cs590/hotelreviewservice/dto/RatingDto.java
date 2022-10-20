64f9f0c185ce7f9289a7f9fb59588f5ebc3da675
prabensoti <praben.soti@hotmail.com>
2022-10-19 21:42:43 -0500

    commit

---
+++ src/main/java/edu/miu/cs590/hotelreviewservice/dto/RatingDto.java
@@ -0,0 +1,37 @@
+package edu.miu.cs590.hotelreviewservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import edu.miu.cs590.hotelreviewservice.entity.Rating;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RatingDto {
+
+    private ObjectId id;
+    private String userId;
+
+    private String hotelId;
+
+    private int rating;
+    private String comment;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private Date dateTime;
+
+
+}
+
+
